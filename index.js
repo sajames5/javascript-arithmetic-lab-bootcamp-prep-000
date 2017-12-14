a12b26cb6c6e7d73c0d95d91cab4da7f95aca58d
Stephanie <s.a.james5@gmail.com>
2017-12-14 18:28:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,4 @@ function divide(a,b) {return a / b}
 function inc(a) {return a + 1}   
 function dec(a) {return a - 1}
 function makeInt(a) {return a}
-var number = 10 
-function multiply()
+var number = 
